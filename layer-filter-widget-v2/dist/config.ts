caeaadb4cab0a8769b404ae46b604c5b699dadc7
dahalkgit <dahalkaustuv@gmail.com>
2025-08-14 10:36:34 +1000

    Update config.ts

--- layer-filter-widget-v2/dist/config.ts
+++ layer-filter-widget-v2/dist/config.ts
@@ -1,7 +1,9 @@
 import { ImmutableObject } from 'seamless-immutable'
 
 export interface Config {
+  /** DataSource ID of the layer to filter */
   layerId: string
+  /** Whitelisted field names selectable in the widget */
   allowedFields: string[]
 }
 
